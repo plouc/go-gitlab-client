49712e87f6a273142a4484984dc36535f09a26cb
Raphaël Benitte <benitteraphael@gmail.com>
2013-07-09 22:51:07 +0200

    Move activity related func to different file for clarity

--- gitlab.go
+++ gitlab.go
@@ -2,7 +2,6 @@
 package gogitlab
 
 import (
-	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -85,24 +84,6 @@ type Hook struct {
 	CreatedAtRaw string `json:"created_at,omitempty"`
 }
 
-type ActivityFeed struct {
-	Title   string        `xml:"title"json:"title"`
-	Id      string        `xml:"id"json:"id"`
-	Link    []Link        `xml:"link"json:"link"`
-	Updated time.Time     `xml:"updated,attr"json:"updated"`
-	Entry   []*FeedCommit `xml:"entry"json:"entries"`
-}
-
-type FeedCommit struct {
-	Id      string    `xml:"id"json:"id"`
-	Title   string    `xml:"title"json:"title"`
-	Link    []Link    `xml:"link"json:"link"`
-	Updated time.Time `xml:"updated"json:"updated"`
-	Author  Person    `xml:"author"json:"author"`
-	Summary string    `xml:"summary"json:"summary"`
-	//<media:thumbnail width="40" height="40" url="https://secure.gravatar.com/avatar/7070eab7c6206530d3b7820362227fec?s=40&amp;d=mm"/>
-}
-
 type Link struct {
 	Rel  string `xml:"rel,attr,omitempty"json:"rel"`
 	Href string `xml:"href,attr"json:"href"`
@@ -148,41 +129,4 @@ func (g *Gitlab) buildAndExecRequest(method string, url string) ([]byte, error) 
 	}
 
 	return contents, err
-}
-
-func (g *Gitlab) Activity() (ActivityFeed, error) {
-
-	url := g.BaseUrl + dasboard_feed_path + "?private_token=" + g.Token
-	fmt.Println(url)
-
-	contents, err := g.buildAndExecRequest("GET", url)
-	if err != nil {
-		fmt.Println("%s", err)
-	}
-
-	var activity ActivityFeed
-	err = xml.Unmarshal(contents, &activity)
-	if err != nil {
-		fmt.Println("%s", err)
-	}
-
-	return activity, err
-}
-
-func (g *Gitlab) RepoActivityFeed(feedPath string) ActivityFeed {
-
-	url := g.BaseUrl + g.RepoFeedPath + "?private_token=" + g.Token
-
-	contents, err := g.buildAndExecRequest("GET", url)
-	if err != nil {
-		fmt.Println("%s", err)
-	}
-
-	var activity ActivityFeed
-	err = xml.Unmarshal(contents, &activity)
-	if err != nil {
-		fmt.Println("%s", err)
-	}
-
-	return activity
 }
