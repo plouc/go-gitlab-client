d681161a120b79deaba5cb12dc2a9ba1f34edafb
Oleg Bovykin <oleg.bovykin@gmail.com>
2014-03-28 20:30:18 +0400

    Added user methods (except create and update) with tests

--- users_test.go
+++ users_test.go
@@ -8,6 +8,20 @@ import (
 	"testing"
 )
 
+func TestUsers(t *testing.T) {
+	stub, err := ioutil.ReadFile("stubs/users/index.json")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(stub))
+	}))
+	defer ts.Close()
+
+	gitlab := NewGitlab(ts.URL, "", "")
+	users, err := gitlab.Users()
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(users), 2)
+}
+
 func TestUser(t *testing.T) {
 	stub, err := ioutil.ReadFile("stubs/users/show.json")
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -32,4 +46,30 @@ func TestUser(t *testing.T) {
 	assert.Equal(t, user.CreatedAt, "2001-01-01T00:00:00Z")
 	assert.Equal(t, user.ExternUid, "uid=plouc")
 	assert.Equal(t, user.Provider, "ldap")
+}
+
+func TestDeleteUser(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(""))
+	}))
+	defer ts.Close()
+
+	gitlab := NewGitlab(ts.URL, "", "")
+	err := gitlab.DeleteUser("1")
+
+	assert.Equal(t, err, nil)
+}
+
+func TestCurrentUser(t *testing.T) {
+	stub, err := ioutil.ReadFile("stubs/users/current.json")
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(stub))
+	}))
+	defer ts.Close()
+
+	gitlab := NewGitlab(ts.URL, "", "")
+	user, err := gitlab.CurrentUser()
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, user.Username, "john_smith")
 }
