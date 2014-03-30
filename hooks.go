6429736090b58ef09e86db4af03b08286f70bde9
Nurahmadie <nurahmadie@gmail.com>
2014-03-30 07:31:45 +0000

    Add support for parsing web hook payload.

--- hooks.go
+++ hooks.go
@@ -2,7 +2,9 @@ package gogitlab
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/url"
+	"time"
 )
 
 const (
@@ -14,6 +16,58 @@ type Hook struct {
 	Id           int    `json:"id,omitempty"`
 	Url          string `json:"url,omitempty"`
 	CreatedAtRaw string `json:"created_at,omitempty"`
+}
+
+type HookObjAttr struct {
+	Id              int       `json:"id,omitempty"`
+	Title           string    `json:"title,omitempty"`
+	AssigneeId      int       `json:"assignee_id,omitempty"`
+	AuthorId        int       `json:"author_id,omitempty"`
+	ProjectId       int       `json:"project_id,omitempty"`
+	CreatedAt       time.Time `json:"created_at,omitempty"`
+	UpdatedAt       time.Time `json:"updated_at,omitempty"`
+	Position        int       `json:"position,omitempty"`
+	BranchName      string    `json:"branch_name,omitempty"`
+	Description     string    `json:"description,omitempty"`
+	MilestoneId     int       `json:"milestone_id,omitempty"`
+	State           string    `json:"state,omitempty"`
+	IId             int       `json:"iid,omitempty"`
+	TargetBranch    string    `json:"target_branch,omitempty"`
+	SourceBranch    string    `json:"source_branch,omitempty"`
+	SourceProjectId int       `json:"source_project_id,omitempty"`
+	StCommits       string    `json:"st_commits,omitempty"`
+	StDiffs         string    `json:"st_diffs,omitempty"`
+	MergeStatus     string    `json:"merge_status,omitempty"`
+	TargetProjectId int       `json:"target_project_id,omitempty"`
+}
+
+type hRepository struct {
+	Name        string `json:"name,omitempty"`
+	URL         string `json:"url,omitempty"`
+	Description string `json:"description,omitempty"`
+	Homepage    string `json:"homepage,omitempty"`
+}
+
+type hCommit struct {
+	Id        string  `json:"id,omitempty"`
+	Message   string  `json:"message,omitempty"`
+	Timestamp string  `json:"timestamp,omitempty"`
+	URL       string  `json:"url,omitempty"`
+	Author    *Person `json:"author,omitempty"`
+}
+
+type HookPayload struct {
+	Before            string       `json:"before,omitempty"`
+	After             string       `json:"after,omitempty"`
+	Ref               string       `json:"ref,omitempty"`
+	UserId            int          `json:"user_id,omitempty"`
+	UserName          string       `json:"user_name,omitempty"`
+	ProjectId         int          `json:"project_id,omitempty"`
+	Repository        *hRepository `json:"repository,omitempty"`
+	Commits           []hCommit    `json:"commits,omitempty"`
+	TotalCommitsCount int          `json:"total_commits_count,omitempty"`
+	ObjectKind        string       `json:"object_kind,omitempty"`
+	ObjectAttributes  *HookObjAttr `json:"object_attributes,omitempty"`
 }
 
 /*
@@ -153,6 +207,32 @@ func (g *Gitlab) RemoveProjectHook(id, hook_id string) error {
 	_, err = g.buildAndExecRequestRaw("DELETE", url, opaque, nil)
 
 	return err
+}
+
+func ParseHook(payload []byte) (*HookPayload, error) {
+	hp := HookPayload{}
+	if err := json.Unmarshal(payload, &hp); err != nil {
+		return nil, err
+	}
+
+	// Basic sanity check
+	switch {
+	case len(hp.ObjectKind) == 0:
+		// Assume this is a post-receive within repository
+		if len(hp.After) == 0 {
+			return nil, fmt.Errorf("Invalid hook received, commit hash not found.")
+		}
+	case hp.ObjectKind == "issue":
+		fallthrough
+	case hp.ObjectKind == "merge_request":
+		if hp.ObjectAttributes == nil {
+			return nil, fmt.Errorf("Invalid hook received, attributes not found.")
+		}
+	default:
+		return nil, fmt.Errorf("Invalid hook received, payload format not recognized.")
+	}
+
+	return &hp, nil
 }
 
 /*
