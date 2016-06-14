886d0858f220e21aa53a95b0b0a71fb23ed333b1
Stephane Leclercq <leclercq@ekino.com>
2016-06-14 14:53:47 +0200

    feat(project) list/add/edit/rm project variables

--- projects_test.go
+++ projects_test.go
@@ -55,3 +55,63 @@ func TestRemoveProject(t *testing.T) {
 	assert.Equal(t, err, nil)
 	assert.Equal(t, result, true)
 }
+
+func TestListVariables(t *testing.T) {
+	ts, gitlab := Stub("stubs/projects/variables/index.json")
+
+	variables, err := gitlab.ProjectVariables("1")
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(variables), 2)
+	defer ts.Close()
+}
+
+func TestGetVariable(t *testing.T) {
+	ts, gitlab := Stub("stubs/projects/variables/show.json")
+
+	result, err := gitlab.ProjectVariable("1", "Somekey")
+
+	assert.NoError(t, err)
+	assert.Equal(t, result.Key, "somekey")
+	assert.Equal(t, result.Value, "somevalue")
+	defer ts.Close()
+}
+
+func TestAddVariable(t *testing.T) {
+	ts, gitlab := Stub("stubs/projects/variables/show.json")
+	req := Variable{
+		Key:   "somekey",
+		Value: "somevalue",
+	}
+	result, err := gitlab.AddProjectVariable("1", &req)
+
+	assert.NoError(t, err)
+	assert.Equal(t, result.Key, "somekey")
+	assert.Equal(t, result.Value, "somevalue")
+	defer ts.Close()
+}
+
+func TestUpdateVariable(t *testing.T) {
+	ts, gitlab := Stub("stubs/projects/variables/show.json")
+	req := Variable{
+		Key:   "somekey",
+		Value: "somevalue",
+	}
+	result, err := gitlab.UpdateProjectVariable("1", &req)
+
+	assert.NoError(t, err)
+	assert.Equal(t, result.Key, "somekey")
+	assert.Equal(t, result.Value, "somevalue")
+	defer ts.Close()
+}
+
+func TestDeleteVariable(t *testing.T) {
+	ts, gitlab := Stub("stubs/projects/variables/show.json")
+
+	result, err := gitlab.DeleteProjectVariable("1", "somekey")
+
+	assert.NoError(t, err)
+	assert.Equal(t, result.Key, "somekey")
+	assert.Equal(t, result.Value, "somevalue")
+	defer ts.Close()
+}
