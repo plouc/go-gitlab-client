ccb2e4a786222b61f8e9ade782814447544d8c35
clchen <clchen@leyantech.com>
2016-09-05 18:19:20 +0800

    fix codiny style

--- projects.go
+++ projects.go
@@ -6,15 +6,15 @@ import (
 )
 
 const (
-	projects_url         = "/projects?page=:page&per_page=:per_page"                         // Get a list of projects owned by the authenticated user
-	projects_all         = "/projects/all?page=:page&per_page=:per_page"                     // Get a list of all GitLab projects (admin only)
-	projects_search_url  = "/projects/search/:query"           // Search for projects by name
-	project_url          = "/projects/:id"                     // Get a specific project, identified by project ID or NAME
-	project_url_events   = "/projects/:id/events"              // Get project events
-	project_url_branches = "/projects/:id/repository/branches" // Lists all branches of a project
-	project_url_members  = "/projects/:id/members"             // List project team members
-	project_url_member   = "/projects/:id/members/:user_id"    // Get project team member
-	max_page_size = 20
+	projects_url         = "/projects?page=:page&per_page=:per_page"     // Get a list of projects owned by the authenticated user
+	projects_all         = "/projects/all?page=:page&per_page=:per_page" // Get a list of all GitLab projects (admin only)
+	projects_search_url  = "/projects/search/:query"                     // Search for projects by name
+	project_url          = "/projects/:id"                               // Get a specific project, identified by project ID or NAME
+	project_url_events   = "/projects/:id/events"                        // Get project events
+	project_url_branches = "/projects/:id/repository/branches"           // Lists all branches of a project
+	project_url_members  = "/projects/:id/members"                       // List project team members
+	project_url_member   = "/projects/:id/members/:user_id"              // Get project team member
+	max_page_size        = 20
 )
 
 type Member struct {
@@ -82,7 +82,7 @@ func projects(u string, g *Gitlab) ([]*Project, error) {
 		url := g.ResourceUrl(
 			u,
 			map[string]string{
-				":page": strconv.FormatInt(page, 10),
+				":page":     strconv.FormatInt(page, 10),
 				":per_page": strconv.FormatInt(max_page_size, 10)})
 
 		var pageProjects []*Project
