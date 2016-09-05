3ed736168f337e5477311e41d6e39372946db398
clchen <clchen@leyantech.com>
2016-09-05 18:12:50 +0800

    fix unittest

--- gitlab_test.go
+++ gitlab_test.go
@@ -8,6 +8,8 @@ import (
 func TestResourceUrl(t *testing.T) {
 	gitlab := NewGitlab("http://base_url/", "api_path", "token")
 
-	assert.Equal(t, gitlab.ResourceUrl(projects_url, nil), "http://base_url/api_path/projects?private_token=token")
+	assert.Equal(t, gitlab.ResourceUrl(
+		projects_url, map[string]string{":page":"1", ":per_page":"20"}),
+		"http://base_url/api_path/projects?page=1&per_page=20&private_token=token")
 	assert.Equal(t, gitlab.ResourceUrl(project_url, map[string]string{":id": "123"}), "http://base_url/api_path/projects/123?private_token=token")
 }
