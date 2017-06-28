0fb1e035878aba719e4f7126658b92c1b5514430
Kevin Stock <kevinstock@tantalic.com>
2017-06-28 10:39:11 -0700

    Adds support for the GitLab Groups API
    APIs supported (https://docs.gitlab.com/ce/api/groups.html)
    	- List groups
    	- List a group's projects
    	- List a group's members
    	- Details of a group
    	- New group
    	- Update group
    	- Remove group

---
+++ groups_test.go
@@ -0,0 +1,111 @@
+package gogitlab
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGroups(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/index.json")
+	defer ts.Close()
+
+	groups, err := gitlab.Groups()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(groups), 2)
+	assert.Equal(t, groups[0].Id, 1)
+	assert.Equal(t, groups[0].Name, "Foobar Group")
+	assert.Equal(t, groups[0].Path, "foo-bar")
+	assert.Equal(t, groups[1].ParentId, 1)
+	assert.Equal(t, groups[1].FullPath, "foo-bar/another")
+}
+func TestGroup(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/show.json")
+	defer ts.Close()
+
+	group, err := gitlab.Group("2")
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, group.Id, 2)
+	assert.Equal(t, group.Name, "Another Group")
+	assert.Equal(t, group.Path, "another")
+	assert.Equal(t, group.ParentId, 1)
+	assert.Equal(t, group.FullPath, "foo-bar/another")
+}
+
+func TestAddGroup(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/add.json")
+	defer ts.Close()
+
+	result, err := gitlab.AddGroup(&Group{
+		Name:                 "New Group",
+		Path:                 "new-group",
+		Description:          "A new group added for testing",
+		Visibility:           "internal",
+		LfsEnabled:           true,
+		RequestAccessEnabled: true,
+	})
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, result.Name, "New Group")
+	assert.Equal(t, result.Path, "new-group")
+	assert.Equal(t, result.Description, "A new group added for testing")
+	assert.Equal(t, result.Visibility, "internal")
+	assert.Equal(t, result.LfsEnabled, true)
+	assert.Equal(t, result.RequestAccessEnabled, true)
+}
+
+func TestUpdateGroup(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/update.json")
+	defer ts.Close()
+
+	result, err := gitlab.UpdateGroup("1", &Group{
+		Name:        "Updated Name",
+		Description: "A new group description",
+	})
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, result.Name, "Updated Name")
+	assert.Equal(t, result.Description, "A new group description")
+
+}
+
+func TestRemoveGroup(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/remove.json")
+	defer ts.Close()
+
+	result, err := gitlab.RemoveGroup("1")
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, result, true)
+}
+
+func TestGroupProjects(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/projects/index.json")
+	defer ts.Close()
+
+	projects, err := gitlab.GroupProjects("1")
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(projects), 2)
+	assert.Equal(t, projects[1].Id, 6)
+	assert.Equal(t, projects[1].Name, "Puppet")
+	assert.Equal(t, projects[1].DefaultBranch, "master")
+	assert.Equal(t, projects[1].SshRepoUrl, "git@example.com:brightbox/puppet.git")
+	assert.Equal(t, projects[1].Owner.Name, "Brightbox")
+}
+
+func TestGroupMembers(t *testing.T) {
+	ts, gitlab := Stub("stubs/groups/members/index.json")
+	defer ts.Close()
+
+	members, err := gitlab.GroupMembers("1")
+
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(members), 2)
+	assert.Equal(t, members[0].Id, 1)
+	assert.Equal(t, members[0].Username, "raymond_smith")
+	assert.Equal(t, members[0].Name, "Raymond Smith")
+	assert.Equal(t, members[1].State, "active")
+	assert.Equal(t, members[1].CreatedAt, "2012-10-22T14:13:35Z")
+}
