a25bbc72dcd6544811e0c4f3ef4e2c1705391648
Kevin Stock <kevin@toolhouse.com>
2017-06-30 08:16:43 -0700

    Fixes invalid struct field tag xml/json pairs
    Previously the xml and json struct tags pairs were not separated by spaces which is not compatible with reflect.StructTag.Get.

--- events.go
+++ events.go
@@ -7,30 +7,30 @@ import (
 )
 
 type Person struct {
-	Name  string `xml:"name"json:"name"`
-	Email string `xml:"email"json:"email"`
+	Name  string `xml:"name" json:"name"`
+	Email string `xml:"email" json:"email"`
 }
 
 type Link struct {
-	Rel  string `xml:"rel,attr,omitempty"json:"rel"`
-	Href string `xml:"href,attr"json:"href"`
+	Rel  string `xml:"rel,attr,omitempty" json:"rel"`
+	Href string `xml:"href,attr" json:"href"`
 }
 
 type ActivityFeed struct {
-	Title   string        `xml:"title"json:"title"`
-	Id      string        `xml:"id"json:"id"`
-	Link    []Link        `xml:"link"json:"link"`
-	Updated time.Time     `xml:"updated,attr"json:"updated"`
-	Entries []*FeedCommit `xml:"entry"json:"entries"`
+	Title   string        `xml:"title" json:"title"`
+	Id      string        `xml:"id" json:"id"`
+	Link    []Link        `xml:"link" json:"link"`
+	Updated time.Time     `xml:"updated,attr" json:"updated"`
+	Entries []*FeedCommit `xml:"entry" json:"entries"`
 }
 
 type FeedCommit struct {
-	Id      string    `xml:"id"json:"id"`
-	Title   string    `xml:"title"json:"title"`
-	Link    []Link    `xml:"link"json:"link"`
-	Updated time.Time `xml:"updated"json:"updated"`
-	Author  Person    `xml:"author"json:"author"`
-	Summary string    `xml:"summary"json:"summary"`
+	Id      string    `xml:"id" json:"id"`
+	Title   string    `xml:"title" json:"title"`
+	Link    []Link    `xml:"link" json:"link"`
+	Updated time.Time `xml:"updated" json:"updated"`
+	Author  Person    `xml:"author" json:"author"`
+	Summary string    `xml:"summary" json:"summary"`
 	//<media:thumbnail width="40" height="40" url="https://secure.gravatar.com/avatar/7070eab7c6206530d3b7820362227fec?s=40&amp;d=mm"/>
 }
 
